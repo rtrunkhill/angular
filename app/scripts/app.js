028aaec59195c51694bc96c156f20f7ed6d6679d
rtrunkhill <rtrunkhill@gmail.com>
2017-07-13 15:08:57 -0700

    angcp-3-assignment complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,11 +9,15 @@
     .state('landing', {
         url: '/',
         templateUrl: '/templates/landing.html'
-      })
+    })
     .state('album', {
         url: '/album',
         templateUrl: '/templates/album.html'
-      });
+    })
+    .state('collection', {
+        url: '/collection',
+        templateUrl: 'templates/collection.html'
+    });
   }
 
   angular.module('blocJams', ['ui.router']);
