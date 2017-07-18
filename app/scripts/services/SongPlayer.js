9e6bbd46d04b870a226d183cb7a3214e10d46143
rtrunkhill <rtrunkhill@gmail.com>
2017-07-17 23:25:10 -0700

    angcp-7-assignment complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,7 +1,8 @@
 (function() {
   function SongPlayer() {
     var SongPlayer = {};
-
+    //        @desc current song object set to null when page loads
+    //        @type {Object}
     var currentSong = null;
     /**
     * @desc Buzz object audio file
@@ -27,21 +28,40 @@
       currentSong = song;
     };
 
+    //  @function playSong
+    //  @desc plays song and sets song.playing to true so album.html changes play/pause icon
+    //  @param {Object} song
+    var playSong = function(song) {
+      currentBuzzObject.play();
+      song.playing = true;
+    }
+
+    //  @function pauseSong
+    //  @desc pauses song and sets song.playing to false so album.html changes play/pause icon
+    //  @param {Object} song
+    var pauseSong = function(song) {
+      currentBuzzObject.pause();
+      song.playing = false;
+    }
+
+    //  @function SongPlayer.play(song)
+    //  @desc plays a song from the beginning if the song has not already started and continues playing the song from where it left off if not
+    //  @params {Object} song
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
       } else if (currentSong === song) {
         if (currentBuzzObject.isPaused()) {
-          currentBuzzObject.play();
+          playSong(song);
         }
       }
     };
-
+    //  @function SongPlayer.pause(song)
+    //  @desc pauses a song at its current time point
+    //  @params {Object} song    
     SongPlayer.pause = function(song) {
-      currentBuzzObject.pause();
-      song.playing = false;
+      pauseSong(song);
     };
 
     return SongPlayer;
